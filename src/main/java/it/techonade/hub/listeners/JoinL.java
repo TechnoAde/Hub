320da511070befa2c03192ece9129f54a6a59c27
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 22:26:43 +0100

    Small Fix

--- src/main/java/it/techonade/hub/listeners/JoinL.java
+++ src/main/java/it/techonade/hub/listeners/JoinL.java
@@ -24,8 +24,8 @@ public class JoinL implements Listener {
     @EventHandler
     public void joinA(PlayerJoinEvent e) {
         bookPacket.openBook(e.getPlayer());
-        if(plugin.getConfig().getString("message.joinbc") != null || !plugin.getConfig().getString("message.joinbc").equals("")) Bukkit.broadcastMessage(Patterns.colorPatterns(plugin.getConfig().getString("message.joinbc").replaceAll("%player%",e.getPlayer().getName()))); else Bukkit.getLogger().info("message.joinbc non è stato definito");
-        if(plugin.getConfig().getString("message.privatemsg") != null) plugin.getConfig().getStringList("message.privatemsg").forEach(t -> e.getPlayer().sendMessage(t));
+        if(plugin.getConfig().getString("messages.joinbc") != null || !plugin.getConfig().getString("messages.joinbc").equals("")) Bukkit.broadcastMessage(Patterns.colorPatterns(plugin.getConfig().getString("messages.joinbc").replaceAll("%player%",e.getPlayer().getName()))); else Bukkit.getLogger().info("messages.joinbc non è stato definito");
+        if(plugin.getConfig().getString("messages.privatemsg") != null) plugin.getConfig().getStringList("messages.privatemsg").forEach(t -> e.getPlayer().sendMessage(t));
         e.getPlayer().getInventory().setItem(8, ItemsL.PLAYER_SHOWED.getItem());
         e.getPlayer().getInventory().setItem(6, ItemsL.ENDERBUTT.getItem());
         Bukkit.getOnlinePlayers().stream().parallel().filter(p -> p.getInventory().contains(ItemsL.PLAYER_HIDED.getItem())).forEach(p -> p.hidePlayer(plugin,e.getPlayer()));
