24ac3dd4757d757f1549eba856f199c52c0e4356
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 21:43:05 +0100

    Added actionbar message and check if shooter is already mounted in enderpearl

--- src/main/java/it/techonade/hub/listeners/ProjectileL.java
+++ src/main/java/it/techonade/hub/listeners/ProjectileL.java
@@ -1,6 +1,10 @@
 package it.techonade.hub.listeners;
 
 import it.techonade.hub.Main;
+import it.techonade.hub.utils.Patterns;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Sound;
 import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -11,10 +15,7 @@ import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class ProjectileL implements Listener {
 
-    private final Main plugin;
-
     public ProjectileL(Main plugin) {
-        this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
@@ -23,7 +24,14 @@ public class ProjectileL implements Listener {
         if(e.getEntity() instanceof Projectile && e.getEntity() instanceof EnderPearl) {
             final Projectile projectile = (Projectile) e.getEntity();
             final Player shooter = (Player) projectile.getShooter();
-            if(shooter != null) { projectile.addPassenger(shooter); }
+            if(shooter != null) {
+                if(!shooter.isInsideVehicle()) {
+                    projectile.addPassenger(shooter);
+                } else {
+                    shooter.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(Patterns.colorPatterns(Main.plugin.getConfig().getString("enderbutt.alreadyFlying"))));
+                    shooter.playSound(shooter, Sound.ENTITY_VILLAGER_NO, 0.5F, 1F);
+                }
+            }
         }
     }
 
